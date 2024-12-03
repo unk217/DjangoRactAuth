61119ede6c337aa52bfa5e0fad8a5f4af80841ce
UNK <unk00@hotmail.com>
2024-12-03 14:52:15 -0500

    Authentication with JWT token

---
+++ backend/base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from django.contrib.auth.models import User
+from .models import Note
+from .serializer import NoteSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_notes(request):
+    user = request.user
+    notes = Note.objects.filter(owner=user)
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+    
